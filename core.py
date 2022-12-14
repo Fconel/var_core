0b5d48a1729e819bd79ae2772533f17555d05a46
Francisco Onel <onel.francisco@gmail.com>
2022-12-14 01:28:52 -0300

    add decay and refactory HVar for multiple assets support

--- core.py
+++ core.py
@@ -2,43 +2,70 @@ import numpy as np
 from datetime import datetime
 import pandas_datareader.data as pdr
 import pandas as pd
+import scipy.stats as stats
 
-def get_returns(historical_prices,log_retun=False):
-    """
-        retuns and logreturns
-    """
+def get_returns(prices,log_retun=False):
+    
     if log_retun:
-        return np.log(historical_prices/ historical_prices.shift(1)).dropna()
+        return np.log(prices/ prices.shift(1)).dropna()
 
     else:
-        return historical_prices.pct_change().dropna()
+        return prices.pct_change().dropna()
 
-def HVar(tickers, start_date, end_date, time_interval,log_retun=False):
+def HVar(tickers, start_date, end_date, time_interval,log_retun=False,alpha=0.05,decay=False,decay_rate=0.95):
 
-    historical_prices = pd.DataFrame()
-    for t in tickers:
-        historical_prices[t] = pdr.YahooDailyReader(t, interval=time_interval , start=start_date,end=end_date).read()['Adj Close']
+    assets_prices = pdr.YahooDailyReader(list(tickers.keys()), interval=time_interval , start=start_date,end=end_date).read()['Adj Close']
+
+    assets_value = assets_prices.iloc[:, 0:]  * list(tickers.values())
+    assets_returns = get_returns(assets_prices)
+    portfolio_weights = assets_value.div(assets_value.sum(axis = 1), axis=0).tail(-1)
+    on_date = assets_prices.index.max()
+    portfolio_value = assets_value.loc[on_date].sum()
+   
+    if not portfolio_weights.index.equals(assets_returns.index):
+        '''
+        add safeguards here
+        '''
+        raise Exception("no hay retornos para todos los pesos")
+
+    #by portfolio
     
-    historical_returns = get_returns(historical_prices)
+    return_portfolio = assets_returns.mul(portfolio_weights.values).sum(axis = 1).dropna().reset_index(drop=True)
+    return_portfolio_sorted = return_portfolio.sort_values()
 
-    alpha = 0.01
-    num_shares = 5000
-    on_date = historical_prices.index.max()
+    xth_smallest_rate = return_portfolio_sorted.quantile(alpha, interpolation="lower")
+    expected_shortfall = return_portfolio_sorted[return_portfolio_sorted < xth_smallest_rate].dropna().mean()
+    mean_return_rate = return_portfolio_sorted.mean()
+    abs_VaR = portfolio_value * xth_smallest_rate
+    
+    #by asset
+    '''
+    Soon add metrics by asset 
+    '''
 
-    for t in tickers:
+    if decay:
 
-        share_price = historical_prices[t].loc[on_date]
-        # No assumption involved use the theoretical distribution
-        daily_return_rates = historical_returns[t].dropna().sort_values().reset_index(drop=True)
+        Decay_vector = [(decay_rate**(i-1) * (1-decay_rate))/(1-decay_rate**len(return_portfolio)) for i in range(1, len(return_portfolio)+1)] 
+        return_portfolio_weighted = return_portfolio[::-1]
+        decay_returns = pd.DataFrame({'returns': return_portfolio_weighted,'decay': Decay_vector}).sort_values(by='returns')
+        decay_returns['cumulative'] = decay_returns.decay.cumsum()
+
+        decay_returns = decay_returns.reset_index().drop(columns=['index'])
+        decay_returns[decay_returns.cumulative <= 0.05].returns.idxmax()
         
+        index=(decay_returns[decay_returns.cumulative <= 0.05].returns.idxmax())
+        
+        xp = decay_returns.loc[index:index+1, 'cumulative'].values
+        fp = decay_returns.loc[index:index+1, 'returns'].values
+        VaR_weighted = np.interp(0.05, xp, fp) 
+        
+        return {"Portfolio Value": portfolio_value, "absolute Age-weighted VaR ":portfolio_value*VaR_weighted, "Age-weighted VaR is " :VaR_weighted,'Expected shortfall':expected_shortfall}
 
-        xth = int(np.floor(0.01*len(daily_return_rates))) - 1
-        xth_smallest_rate = daily_return_rates[xth]
+    return {"Portfolio Value": portfolio_value, "absolute VaR":abs_VaR, "perc" :xth_smallest_rate,'Expected shortfall':expected_shortfall}
 
-        mean_return_rate = daily_return_rates.mean()
-
-        rel_VaR = num_shares * share_price * (mean_return_rate - xth_smallest_rate)
-        abs_VaR = -num_shares * share_price * xth_smallest_rate
-
-        print("The estimated relative VaR and absolute VaR of an investment of", num_shares, "shares of", t, "on", on_date, "with price $", round(share_price,2), "per share is $", round(rel_VaR,2), "and $", round(abs_VaR,2), "respectively.")
-
+def PVar(tickers, start_date, end_date, time_interval,log_retun=False,alpha=0.05):
+    
+    '''
+    soon
+    '''
+    return None
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import core
 from datetime import datetime
 
-tickers = ['GOOG','MSFT']
-start_date = datetime.strptime('2018-01-01', '%Y-%m-%d')
-end_date = datetime.strptime('2021-03-10', '%Y-%m-%d')
-time_interval='d'
+def main():
+    tickers = {'GOOG':500,'MSFT':600}
+    start_date = datetime.strptime('2018-01-01', '%Y-%m-%d')
+    end_date = datetime.strptime('2021-03-10', '%Y-%m-%d')
+    time_interval='d'
+
+    value_at_risk=core.HVar(tickers, start_date, end_date, time_interval, alpha = 0.05,decay=True,decay_rate=0.95)
+
+    print(value_at_risk)
 
 if __name__ == "__main__":
 
-    core.HVar(tickers, start_date, end_date, time_interval)
-
+    main()
